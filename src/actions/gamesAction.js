2753384ca5ef7f03d32fc7fc3a579c541837be76
Roeck <rons9n@gmail.com>
2020-12-04 18:05:56 -0500

    fetching other games

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,14 +1,19 @@
 import axios from 'axios';
-import { popularGamesURL } from '../api';
+import { popularGamesURL, upcomingGamesURL, newGamesURL } from '../api';
 
 // Action creator
 export const loadGames = () => async (dispatch) => {
   // Fetch axios
   const popularData = await axios.get(popularGamesURL());
+  const newGamesData = await axios.get(newGamesURL());
+  const upcomingData = await axios.get(upcomingDataURL());
+
   dispatch({
     type: 'FETCH_GAMES',
     payload: {
-      popular: popularData.data.results
+      popular: popularData.data.results,
+      upcoming: upcomingData.data.results,
+      newGames: newGamesData.data.results
     }
   });
 };
