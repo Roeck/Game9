1137cb8aaebbbe28d3a5e7cd850853901741c6df
Roeck <rons9n@gmail.com>
2020-12-13 16:41:48 -0500

    return image bux fix

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -3,132 +3,53 @@ import React from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 //Redux
-import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { loadDetail } from '../actions/detailAction';
+import { Link } from 'react-router-dom';
 import { smallImage } from '../util';
-//IMAGES
+// import { popup } from '../animations';
 
-//Star Images
-
-const GameDetail = ({ pathId }) => {
-  const history = useHistory();
-
-  //Exit Detail
-  const exitDetailHander = (e) => {
-    const element = e.target;
-    if (element.classList.contains('shadow')) {
-      document.body.style.overflow = 'auto';
-      history.push('/');
-    }
+const Game = ({ name, released, image, id }) => {
+  const stringPathId = id.toString();
+  //Load Detail Handler
+  const dispatch = useDispatch();
+  const loadDetailHandler = () => {
+    document.body.style.overflow = 'hidden';
+    dispatch(loadDetail(id));
   };
 
-  //Data
-  const { screen, game, isLoading } = useSelector((state) => state.detail);
   return (
-    <>
-      {!isLoading && (
-        <CardShadow className='shadow' onClick={exitDetailHander}>
-          <Detail layoutId={pathId}>
-            <Stats>
-              <Info>
-                <h3>Platforms</h3>
-                <Platforms>
-                  {game.platforms.map((data) => (
-                    <h3 key={data.platform.id}>{data.platform.name}</h3>
-                  ))}
-                </Platforms>
-              </Info>
-            </Stats>
-            <Media>
-              <motion.img
-                layoutId={`image ${pathId}`}
-                src={smallImage(game.background_image, 1280)}
-                alt={game.background_image}
-              />
-            </Media>
-            <Description>
-              <p>{game.description_raw}</p>
-            </Description>
-            <div className='gallery'>
-              {screen.results.map((screen) => (
-                <img
-                  src={smallImage(screen.image, 1280)}
-                  key={screen.id}
-                  alt={screen.image}
-                />
-              ))}
-            </div>
-          </Detail>
-        </CardShadow>
-      )}
-    </>
+    <StyledGame
+      initial='hidden'
+      animate='show'
+      layoutId={stringPathId}
+      onClick={loadDetailHandler}
+    >
+      <Link to={`/game/${id}`}>
+        <motion.h3 layoutId={`title ${stringPathId}`}>{name}</motion.h3>
+        <p>{released}</p>
+        <motion.img
+          layoutId={`image ${stringPathId}`}
+          src={smallImage(image, 640)}
+          alt={name}
+        />
+      </Link>
+    </StyledGame>
   );
 };
 
-const CardShadow = styled(motion.div)`
-  width: 100%;
-  min-height: 100vh;
-  overflow-y: scroll;
-  background: rgba(0, 0, 0, 0.5);
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 5;
-  &::-webkit-scrollbar {
-    width: 0.5rem;
-  }
-  &::-webkit-scrollbar-thumb {
-    background-color: #ff7676;
-  }
-  &::-webkit-scrollbar-track {
-    background: white;
-  }
-`;
-
-const Detail = styled(motion.div)`
-  width: 80%;
-  border-radius: 1rem;
-  padding: 2rem 5rem;
-  background: white;
-  position: absolute;
-  left: 10%;
-  color: black;
-  z-index: 10;
-  img {
-    width: 100%;
-  }
-`;
-
-const Stats = styled(motion.div)`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  img {
-    width: 2rem;
-    height: 2rem;
-    display: inline;
-  }
-`;
-const Info = styled(motion.div)`
+const StyledGame = styled(motion.div)`
+  min-height: 30vh;
+  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
   text-align: center;
-`;
-const Platforms = styled(motion.div)`
-  display: flex;
-  justify-content: space-evenly;
-  img {
-    margin-left: 3rem;
-  }
-`;
-
-const Media = styled(motion.div)`
-  margin-top: 5rem;
+  border-radius: 1rem;
+  cursor: pointer;
+  overflow: hidden;
   img {
     width: 100%;
+    height: 40vh;
+    object-fit: cover;
   }
 `;
 
-const Description = styled(motion.div)`
-  margin: 5rem 0rem;
-`;
-
-export default GameDetail;
+export default Game;
